d734309b38fdfccf4306a0217f0198c77f404ab8
Batuhan Gökalp <bbatuhangokalp@gmail.com>
2025-07-09 12:28:52 +0300

    edited

--- src/pages/dashboard/UserChat/RenderAudio.js
+++ src/pages/dashboard/UserChat/RenderAudio.js
@@ -1,25 +1,26 @@
-import React from 'react'
-import { FileTypeId, findFileType } from '../../../helpers/chatConstants';
+import React from "react";
+import { FileTypeId, findFileType } from "../../../helpers/chatConstants";
 
 export default function RenderAudio({ chatFile, url }) {
-    return (
-        <>
-            {chatFile &&  FileTypeId.Audio?.includes(findFileType(chatFile?.type)) && (
-                <div className="text-center">
-                    <audio controls>
-                        <source src={URL.createObjectURL(chatFile)} type="audio/ogg" />
-                    </audio>
-                </div>
-            )}
-            {
-                url &&
-                <div className="text-center">
-                    <audio controls>
-                        <source src={url} type="audio/mp3" />
-                    </audio>
-                </div>
-            }
-
-        </>
-    )
+  return (
+    <>
+      {chatFile && FileTypeId.Audio?.includes(findFileType(chatFile?.type)) && (
+        <div className="text-center">
+          <audio controls>
+            <source
+              src={URL.createObjectURL(chatFile)}
+              type={chatFile.type || "audio/ogg"}
+            />
+          </audio>
+        </div>
+      )}
+      {url && (
+        <div className="text-center">
+          <audio controls>
+            <source src={url} type="audio/mp3" />
+          </audio>
+        </div>
+      )}
+    </>
+  );
 }
